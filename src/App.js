d7373257f05443b434e1dfbd3838181d15df3a72
Natalie Murphy <Natalie.murphy@igrafx.com>
2022-10-30 21:30:39 -0700

    moved env to docker compose

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {`Hello ${process.env.REACT_APP_NAME} baby lol `}
+          {`Hello ${process.env.REACT_APP_NAME} baby hehe`}
         </p>
         <a
           className="App-link"
